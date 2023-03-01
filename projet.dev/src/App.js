71f2a773435d532081164f8405851c58131afd68
chaimaammeri <chaimaammerii24@gmail.com>
2023-03-01 10:40:59 +0100

    App.js est modifié

--- projet.dev/src/App.js
+++ projet.dev/src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-   
-      <h1 className="title">this is my react app </h1>
+       <div>
+        <h1>helloooo</h1>
+        <h1 className="title">this is my react app </h1>
+       </div>
   );
 }
 

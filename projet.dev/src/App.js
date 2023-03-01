11e782251c3cdb1c45c94e9265ed898fb7050105
chaimaammeri <chaimaammerii24@gmail.com>
2023-03-01 09:50:18 +0100

    borderrr

--- projet.dev/src/App.js
+++ projet.dev/src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
    
-    <h1 className="title">this is my react app </h1> 
+      <h1 className="title">this is my react app </h1>;
+      <h1 className="title">this is my react app </h1> ;
   );
 }
 
